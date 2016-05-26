20a3f5a9519616ca6e417c8cb7a273989b4fb57f
Laura <lauracloquell@gmail.com>
2016-05-26 11:59:15 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,13 +3,11 @@ library(reshape2)
 filename <- "getdata_dataset.zip"
 
 ## Download and unzip the dataset:
-if (!file.exists(filename)){
-  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
-  download.file(fileURL, filename, method="curl")
-}  
-if (!file.exists("UCI HAR Dataset")) { 
-  unzip(filename) 
-}
+if (!file.exists("./project")){dir.create("./project")}
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" 
+download.file(fileURL, "./project/Dataset.zip", method="curl")
+unzip(zipfile="./project/Dataset.zip", exdir="./project")  
 
 # Load activity labels + features
 activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
